b6217b9756a793f1b9d65ebf90e87502deddfc10
natishakhadgi <natisha.khadgi@sjsu.edu>
2020-07-20 20:05:44 -0700

    Initial Commit

---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by Natisha Khadgi on 7/20/20.
+//  Copyright © 2020 Natisha Khadgi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
